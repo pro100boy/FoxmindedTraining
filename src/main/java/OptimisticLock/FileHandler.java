c98dc60e45fea2101c81b6e51d44cde027d1577d
PGAL <pavel.galushkin@gmail.com>
2018-11-18 13:45:43 +0200

    OptimisticLock

--- src/main/java/OptimisticLock/FileHandler.java
+++ src/main/java/OptimisticLock/FileHandler.java
@@ -12,20 +12,20 @@ import java.util.stream.Collectors;
 
 public class FileHandler {
     private int version;
-    private final static String filePath = "/home/user";
+    private final static String filePath = "/home/pavel";
     private final static String fileNamePrefix = "file";
     private final static String fileExtension = "txt";
 
-    private String makeFilePath() {
-        return filePath + "/" + fileNamePrefix + version + "." + fileExtension;
+    private Path makeFilePath() {
+        return Paths.get(filePath + "/" + fileNamePrefix + version + "." + fileExtension);
     }
 
-    private String makeFilePath(FileWrapper wrapper) {
-        return filePath + "/" + wrapper.getName();
+    private Path makeFilePath(FileWrapper wrapper) {
+        return Paths.get(filePath + "/" + wrapper.getName());
     }
 
     public FileWrapper readFile() throws IOException {
-        Path path = Paths.get(makeFilePath());
+        Path path = makeFilePath();
 
         return new FileWrapper(
                 path.getFileName().toString(),
@@ -34,37 +34,19 @@ public class FileHandler {
     }
 
     public void saveFile(FileWrapper wrapper) throws IOException {
-        Path path = Paths.get(makeFilePath(wrapper));
+        Path path = makeFilePath(wrapper);
         File file = path.toFile();
         if (file.isFile())
         {
             Files.write(path, Arrays.asList(wrapper.getContent().split(System.lineSeparator())));
-            version += 1;
-            Path target = Paths.get(makeFilePath());
+            ++version;
+            Path target = makeFilePath();
             Files.move(path, target, StandardCopyOption.ATOMIC_MOVE);
             System.out.println("File " + wrapper.getName() + " was wrote");
         }
         else
         {
-            System.err.println("File " + wrapper.getName() + " don't wrote");
-            throw new OptimisticLockException();
+            throw new OptimisticLockException("File " + wrapper.getName() + " already has changed");
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        FileHandler fileHandler1 = new FileHandler();
-        FileWrapper fileWrapper1 = fileHandler1.readFile();
-
-        FileHandler fileHandler2 = new FileHandler();
-        FileWrapper fileWrapper2 = fileHandler2.readFile();
-
-        System.out.println(fileWrapper1);
-        System.out.println(fileWrapper2);
-
-        fileWrapper1.setContent("new content 1");
-        fileHandler1.saveFile(fileWrapper1);
-
-        fileWrapper1.setContent("new content 2");
-        fileHandler1.saveFile(fileWrapper2);
     }
 }
---
+++ src/main/java/OptimisticLock/Main.java
@@ -0,0 +1,22 @@
+package OptimisticLock;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileHandler fileHandler1 = new FileHandler();
+        FileWrapper fileWrapper1 = fileHandler1.readFile();
+
+        FileHandler fileHandler2 = new FileHandler();
+        FileWrapper fileWrapper2 = fileHandler2.readFile();
+
+        System.out.println(fileWrapper1);
+        System.out.println(fileWrapper2);
+
+        fileWrapper1.setContent("new content 1");
+        fileHandler1.saveFile(fileWrapper1);
+
+        fileWrapper1.setContent("new content 2");
+        fileHandler1.saveFile(fileWrapper2);
+    }
+}
